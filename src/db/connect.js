cd074764598f1cd6d2973fe5442193c194b2fd3b
Mohammad Amaan Khan <2019bcs118@sggs.ac.in>
2022-02-17 16:39:12 +0530

    made changes

--- src/db/connect.js
+++ src/db/connect.js
@@ -3,6 +3,5 @@ const mongoose = require("mongoose");
 
 mongoose.connect("mongodb + srv://aman_khanO_o:Amaan%401234@projects.ouxex.mongodb.net/MFG_Data?retryWrites=true&w=majority", {
     useUnifiedTopology: true,
-    useNewUrlParser: true,
     useNewUrlParser: true
 });

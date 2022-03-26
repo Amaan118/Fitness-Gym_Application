953e814c9f5e5674e7fbc10b2a78230970e6907e
Mohammad Amaan Khan <2019bcs118@sggs.ac.in>
2022-03-26 16:26:29 +0530

    fixed bugs

--- public/javascript/cut_btn.js
+++ public/javascript/cut_btn.js
@@ -2,6 +2,6 @@ let msg_cut = document.getElementById('cut');
 
 if (msg_cut) {
     msg_cut.addEventListener('click', function () {
-        this.parentNode.style.display = 'none';
+        this.parentNode.style.visibility = 'hidden';
     });
 }
